5340ecf01c47d273ecdee887a37380d4586e528f
m-nady <mo7md.nady@gmail.com>
2023-07-13 14:10:30 -0700

    qoute

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,17 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints "and that piece of art is useful" - Dora Korpar,
+ *              2015-10-19, followed by a new line, to the standard error.
+ * Return: Always 1 (Error code)
+ */
 int main(void)
 {
-    char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t len = 59; /* length of the string */
-
-    /* write the string to standard error using write() */
-    write(2, str, len);
-
-    return 1;
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	ssize_t len = 59; /* length of the string */
+	/* write the string to standard error using write() */
+	write(2, str, len);
+	return (1);
 }
