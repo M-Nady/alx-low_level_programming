38a16faa5020efd017be111ff7f91b2a794a5e73
m-nady <mo7md.nady@gmail.com>
2023-07-15 14:39:21 -0700

    5-prints alphabet

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-        int n = 0;
+	int n = 0;
 
-        for(n = 0; n <= 9; n++)
-        {
-                printf("%d", n);
-        }
-        printf("\n");
-        return (0);
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
 }
