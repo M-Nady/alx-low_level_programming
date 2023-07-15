94457f5552274b63f1fcf735bdc07be63fb07d8d
m-nady <mo7md.nady@gmail.com>
2023-07-15 14:58:42 -0700

    100-print

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,27 +7,27 @@
  */
 int main(void)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 90; i++)
-    {
-        for (j = i + 1; j < 100; j++)
-        {
-            if (i != j)
-            {
-                putchar((i / 10) + '0');
-                putchar((i % 10) + '0');
-                putchar(',');
-                putchar(' ');
-                putchar((j / 10) + '0');
-                putchar((j % 10) + '0');
-                if (i == 98 && j == 99)
-                    break;
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    putchar('\n');
-    return (0);
+	for (i = 0; i < 90; i++)
+	{
+		for (j = i + 1; j < 100; j++)
+		{
+			if (i != j)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(',');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				if (i == 98 && j == 99)
+					break;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
