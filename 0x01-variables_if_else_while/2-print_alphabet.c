a8a15f29856019da8261abca28ceeac0010a1f99
m-nady <mo7md.nady@gmail.com>
2023-07-15 14:29:41 -0700

    2-print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,14 +9,13 @@
  */
 int main(void)
 {
-        int n = 97;
+	int n = 97;
 
-        while (n <= 122)
-        {
-                putchar(n);
-                n++;
-        }
-        putchar('\n');
-
-        return (0);
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
 }
