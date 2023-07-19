268a7c9f6da86ece776c699f17a45fb15d1d74b6
m-nady <mo7md.nady@gmail.com>
2023-07-19 14:25:11 -0700

    lower

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -14,22 +14,3 @@ void print_alphabet(void)
         }
         _putchar('\n');
 }
-root@ff05b62a00b2:/alx-low_level_programming/0x02-functions_nested_loops# cat 1-alphabet.c ^C
-root@ff05b62a00b2:/alx-low_level_programming/0x02-functions_nested_loops# cat 1-alphabet.c 
-#include "main.h"
-/**
-  * print_alphabet - Prints the alphabet in lowercase.
-  *
-  * Return: Always 0 (Success)
-  */
-void print_alphabet(void)
-{
-        int i;
-
-        for (i = 97; i <= 122; i++)
-        {
-                _putchar(i);
-        }
-        _putchar('\n');
-}
-
