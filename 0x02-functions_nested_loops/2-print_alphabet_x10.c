714c17800c9ebb15ee45e8a2bf21f34cf2e878c2
m-nady <mo7md.nady@gmail.com>
2023-07-21 13:42:01 -0700

    2-print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,15 @@
  */
 void print_alphabet_x10(void)
 {
-        int i;
-        int j;
+	int i;
+	int j;
 
-        for (i = 1; i <= 10; i++)
-        {
-                for (j = 97; j <= 122; j++)
-                {
-                        _putchar(j);
-                }
-                _putchar('\n');
-        }
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
 }
