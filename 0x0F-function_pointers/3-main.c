a845845858478a3fc5ab32cafd1049f28b927930
m-nady <mo7md.nady@gmail.com>
2023-08-18 01:44:33 -0700

    functions pointer

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,28 +1,38 @@
 #include "3-calc.h"
-
 /**
- * main - performs simple operations
- * @argc: argument count
- * @argv: argument vector
- * Return: 1 for success
- */
-
+  *main - check arguments
+  *@argc: argument count
+  *@argv: argument vector.
+  *
+  *Return: error if number of arguments is wrong
+  */
 int main(int argc, char *argv[])
 {
-	register int a, b;
-	int (*fptr)(int, int);
+	int a = 0, b = 0, res = 0;
+	char s;
 
 	if (argc != 4)
 	{
-		printf("Error\n"), exit(98);
+		printf("Error\n");
+		exit(98);
 	}
-	fptr = get_op_func(argv[2]);
-	if (!fptr)
+	/* check if theres only one operator*/
+	if (argv[2][1] != '\0')
 	{
-		printf("Error\n"), exit(99);
+		printf("Error\n");
+		exit(99);
 	}
+
+	s = argv[2][0];
+	if (s != '+' && s != '-' && s != '/' && s != '*' && s != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	printf("%i\n", fptr(a, b));
-	return (1);
+	res = (get_op_func(argv[2]))(a, b);
+	printf("%d\n", res);
+	return (0);
 }
